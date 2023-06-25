997f0999666e14e0119f4e0176e5fe49b6082b80
ejk1058577 <112951378+ejk1058577@users.noreply.github.com>
2023-06-25 17:24:11 +0200

    wall generation

--- src/js/level/floor.js
+++ src/js/level/floor.js
@@ -1,4 +1,4 @@
-import { Actor, Color } from "excalibur";
+import { Actor, CollisionType, Color, Vector } from "excalibur";
 import { Level } from "./level";
 
 export class Floor {
@@ -11,15 +11,14 @@ export class Floor {
         this.scene = scene;
 
         this.bounds = {
-            xmax: 4 * 50 + 4 * 140, //4*wall size + 3 * level size,
-            xmin: 0, //just make this zero
-            ymax: 4 * 50 + 4 * 140,
-            ymin: 0
+            xmax: (4 * 50) + (3 * 4 * 140), //4*wall size + 3 * level size,
+            xmin: 200, //200 seems right idk why
+            ymax: (4 * 50) + (3 * 4 * 140),
+            ymin: 200
         }
 
         this.createGrid();
-
-        
+        this.createWalls();
     }
 
     createGrid() {
@@ -31,7 +30,7 @@ export class Floor {
         ];
 
         this.grid.forEach((arr, i) => {
-            arr = arr.map((v, j) => {
+            this.grid[i] = arr.map((v, j) => {
                 //in case of editing something about the level based on num, access {v}
 
                 let hz = j+1; //j+1 = hz position
@@ -43,15 +42,50 @@ export class Floor {
                     ymin: vt * 50 + (vt-1) * 4 * 140
                 }
 
-                console.log(lvlBounds);
-
                 return new Level(this.scene, lvlBounds);
             });
         });
     }
 
     createWalls() {
+        this.grid.forEach((arr, i) => {
+            arr.forEach((l, j) => {
+                //horizontal walls
+                for (let h = 0; h < 4; h++) {
+                    let pos = new Vector(l.bounds.xmin + (h+1)*140, l.bounds.ymin + 45); //weird 5 offset
+                    let wall = new Wall({}, pos, 0);
+                    this.scene.add(wall);
 
+                    //final row
+                    if (i == 2) {
+                        let pos = new Vector(l.bounds.xmin + (h+1)*140, l.bounds.ymax + 100);
+                        let wall = new Wall({}, pos, 0);
+                        this.scene.add(wall);
+                    }
+                }
+
+                //vertical walls
+                for (let h = 0; h < 4; h++) {
+                    let pos = new Vector(l.bounds.xmin + 45, l.bounds.ymin + (h+1)*140);
+                    let wall = new Wall({}, pos, 1);
+                    this.scene.add(wall);
+
+                    if (j == 2) {
+                        let pos = new Vector(l.bounds.xmax + 100, l.bounds.ymin + (h+1)*140);
+                        let wall = new Wall({}, pos, 1);
+                        this.scene.add(wall);
+                    }
+                }
+            });
+        });
+
+        //last row hz
+        // for (let h = 0; h < 4; h++) {
+        //     let pos = new Vector(this.bounds.xmin + h*140, this.bounds.ymax + 50);
+        //     let wall = new Wall({}, pos, 0);
+        //     this.scene.add(wall);
+        //     console.log(wall.id);
+        // }
     }
 }
 
@@ -59,11 +93,12 @@ class Wall extends Actor {
     or; //0 == hz | 1 == vt, stands for orientation
 
     constructor(options, pos, or) {
-        options.width = this.or == 0 ? 140 : 50;
-        options.height = this.or == 0 ? 50 : 140;
-        options.Color = Color.Black;
-
+        options.width = or == 0 ? 140 : 50;
+        options.height = or == 0 ? 50 : 140;
+        options.color = Color.Black;
         super(options);
+        this.or = or;
         this.pos = pos;
+        this.body.collisionType = CollisionType.Fixed;
     }
 }
