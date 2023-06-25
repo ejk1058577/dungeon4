b1f66c340c4cfa9b785342a6c4c1b4985c9b5333
ejk1058577 <112951378+ejk1058577@users.noreply.github.com>
2023-06-25 02:15:01 +0200

    added fireball

--- src/js/actors/player.js
+++ src/js/actors/player.js
@@ -2,10 +2,25 @@ import {Input, Actor, Color, Vector} from 'excalibur';
 
 export class Player extends Actor {
     inventory;
+    fireballCooldown;
+    fireballLifespan;
+    cooldownActive;
 
     constructor(options) {
         super(options);
         this.pos = new Vector(200, 200);
+
+        this.fireballLifespan = 1600;
+        this.fireballCooldown = 1500;
+        this.cooldownActive = false;
+    }
+
+    onInitialize(_engine) {
+        super.onInitialize(_engine);
+
+        _engine.input.pointers.on('down', (e) => {
+            this.fireball(_engine.input.pointers.primary.lastWorldPos);
+        });
     }
 
     onPreUpdate(_engine, _delta) {
@@ -37,13 +52,33 @@ export class Player extends Actor {
         if (engine.input.keyboard.isHeld(Input.Keys.D)) {
             inputs++;
             xspeed = 150;
-        }   
+        }
 
         if (inputs > 1) {
             xspeed = xspeed/1.414;
             yspeed = yspeed/1.414;
         }
         this.vel = new Vector(xspeed, yspeed);
+    }
+
+    fireball(target) {
+        if (this.cooldownActive) return;
+        this.cooldownActive = true;
+
+        let tarDir = new Vector(target.x - this.pos.x, target.y - this.pos.y).normalize();
+        let angle = tarDir.toAngle();
+        let fb = new Fireball({}, angle, tarDir, this);
+        this.scene.add(fb);
+
+        //cooldown
+        this.scene.engine.clock.schedule(() => {
+            this.cooldownActive = false;
+        }, this.fireballCooldown);
+
+        //lifespan
+        this.scene.engine.clock.schedule(() => {
+            fb.kill();
+        }, this.fireballLifespan);
     }
 }
 
@@ -64,5 +99,21 @@ class Inventory {
 
     calculateEffects() {
 
+    }
+}
+
+class Fireball extends Actor {
+    direction;
+    angle;
+
+    constructor(options, angle, direction, player) {
+        super({
+            width: 30,
+            height: 30,
+            color: Color.Red
+        });
+
+        this.pos = new Vector(player.pos.x + direction.x*64 , player.pos.y + direction.y*64);
+        this.vel = new Vector(direction.x * 256, direction.y * 256);
     }
 }
